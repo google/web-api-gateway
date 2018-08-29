be5cc57714c85b21a26ea42a37213b16ea112b98
Scott Redig <sredig@google.com>
2018-08-29 14:56:35 -0700

    Initial version of the actual server implementation
    Change-Id: I553a4102bba5fa5393f823136247a2e9080b2c4a

--- server/server.go
+++ server/server.go
@@ -16,8 +16,166 @@ limitations under the License.
 
 package main
 
-import "log"
+import (
+	"context"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"runtime"
+	"time"
+	"web-api-gateway/config"
+
+	"golang.org/x/oauth2"
+)
+
+const version = "0.1"
 
 func main() {
-	log.Println("At some point, this will be a server.")
+	flag.Parse()
+
+	log.Println("Reading config file...")
+	mux := createConfigHandler()
+
+	log.Println("Starting server...")
+	// TODO IMPORTANT: Replace with this https.  Don't even offer a non-https endpoint.
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+func createConfigHandler() http.Handler {
+	c, err := config.ReadConfig() // TODO read from file.
+	if err != nil {
+		log.Printf("Error reading config file: %s", err)
+		return ErrorReadingConfig
+	}
+
+	mux := http.NewServeMux()
+	for _, service := range c.Services {
+		for _, account := range service.Accounts {
+			path, handler, err := createAccountHandler(service, account)
+			if err != nil {
+				log.Printf(
+					"Error reading the config, service: %s, account: %s, error: %s",
+					service.ServiceName,
+					account.AccountName,
+					err)
+				return ErrorReadingConfig
+			}
+			mux.Handle(path, handler)
+		}
+	}
+
+	return mux
+}
+
+func createAccountHandler(service *config.Service, account *config.Account) (string, http.Handler, error) {
+	mux := http.NewServeMux()
+
+	{
+		handler, err := createOAuthForwarder(service, account)
+		if err != nil {
+			return "", nil, err
+		}
+		mux.Handle("/forward/", http.StripPrefix("/forward/", handler))
+	}
+
+	mux.Handle("/status", createStatusPage(service, account))
+
+	// TODO: we're assuming that service and account names are valid.  The editing tool validates this
+	// but it should be validated when loading too.
+	basePath := fmt.Sprintf("/service/%s/account/%s/", service.ServiceName, account.AccountName)
+
+	handler, err := wrapWithClientAuth(http.StripPrefix(basePath, mux), account)
+	if err != nil {
+		return "", nil, err
+	}
+	return basePath, handler, nil
+}
+
+func wrapWithClientAuth(handler http.Handler, account *config.Account) (http.Handler, error) {
+	// TODO TEST account.ClientCreds.Protocol and ensure it's what we're expecting here.
+
+	der, err := base64.StdEncoding.DecodeString(account.ClientCreds.PrivateKey)
+	if err != nil {
+		// Don't log error details in case they include info on the secret.
+		return nil, errors.New("error decoding private key from base64")
+	}
+
+	privateKey, err := x509.ParseECPrivateKey(der)
+	if err != nil {
+		// Don't log error details in case they include info on the secret.
+		return nil, errors.New("error parsing private key")
+	}
+
+	return onlyAllowVerifiedRequests(handler, &privateKey.PublicKey, time.Now), nil
+}
+
+func createOAuthForwarder(service *config.Service, account *config.Account) (http.Handler, error) {
+	var endpoint = oauth2.Endpoint{
+		AuthURL:  service.OauthServiceCreds.AuthURL,
+		TokenURL: service.OauthServiceCreds.TokenURL,
+	}
+
+	oauthConf := &oauth2.Config{
+		ClientID:     service.OauthServiceCreds.ClientID,
+		ClientSecret: service.OauthServiceCreds.ClientSecret,
+		Scopes:       service.OauthServiceCreds.Scopes,
+		Endpoint:     endpoint,
+	}
+
+	token := &oauth2.Token{
+		RefreshToken: account.OauthAccountCreds.RefreshToken,
+	}
+
+	transport := &oauth2.Transport{
+		Source: oauthConf.TokenSource(context.Background(), token),
+	}
+
+	domain, err := url.Parse(account.OauthAccountCreds.ServiceURL)
+	if err != nil {
+		return nil, err
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(domain)
+	proxy.Transport = transport
+
+	return proxy, err
+}
+
+func createStatusPage(service *config.Service, account *config.Account) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		status := struct {
+			Version    string
+			GoVersion  string
+			Scopes     []string
+			TokenUrl   string
+			AuthUrl    string
+			ServiceUrl string
+		}{
+			Version:    version,
+			GoVersion:  runtime.Version(),
+			AuthUrl:    service.OauthServiceCreds.AuthURL,
+			TokenUrl:   service.OauthServiceCreds.TokenURL,
+			Scopes:     service.OauthServiceCreds.Scopes,
+			ServiceUrl: account.OauthAccountCreds.ServiceURL,
+		}
+
+		b, err := json.Marshal(status)
+		if err != nil {
+			ErrorEncodingStatusJson.ServeHTTP(w, r)
+			return
+		}
+
+		_, err = w.Write(b)
+		if err != nil {
+			ErrorIO.ServeHTTP(w, r)
+			return
+		}
+	})
 }
